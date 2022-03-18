ef3d1fb37c48d4313517d4f8edb3699ef54a01aa
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-03-18 22:51:00 +0100

    complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,11 @@
 const hamburgerBtn = document.querySelector('.header__clickzone')
 const hamburgerMenu = document.querySelector('.header__menu')
 const body = document.querySelector('body')
+const overlay = document.querySelector('.overlay')
 
 hamburgerBtn.addEventListener('click', ()=>{
     console.log('hello')
     hamburgerMenu.classList.toggle('active')
     body.classList.toggle('noscroll')
+    overlay.classList.toggle('active')
 })
